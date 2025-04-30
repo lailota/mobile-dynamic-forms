e7f3b385d96f7249bef6df5a4cc0b546a278c34b
lailota <laila.guzzon@gmail.com>
2025-04-30 12:20:29 -0300

    Initial Commit

---
+++ DynamicForms/DynamicFormsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicFormsApp.swift
+//  DynamicForms
+//
+//  Created by Laila Guzzon Hussein on 30/04/25.
+//
+
+import SwiftUI
+
+@main
+struct DynamicFormsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
