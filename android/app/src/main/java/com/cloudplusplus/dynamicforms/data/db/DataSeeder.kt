0085fe94b63e88888c36012e0cc0604b299f6b1e
Laila G. Hussein <83671883+lailota@users.noreply.github.com>
2025-05-02 09:25:41 -0300

    Update DataSeeder.kt

--- android/app/src/main/java/com/cloudplusplus/dynamicforms/data/db/DataSeeder.kt
+++ android/app/src/main/java/com/cloudplusplus/dynamicforms/data/db/DataSeeder.kt
@@ -16,26 +16,26 @@ class DataSeeder(
     private val sectionDao: SectionDao
 ) {
     suspend fun seedIfNeeded() = withContext(Dispatchers.IO) {
-        // só seedamos na primeira execução
+        // we only sow in the first execution
         if (fieldDao.getAll().isEmpty()) {
-            // 1) lê o JSON completo como string
+            // 1) reads the complete JSON as a string
             val jsonText = context.assets
                 .open(assetFileName)
                 .bufferedReader()
                 .use { it.readText() }
 
-            // 2) parse para JsonObject
+            // 2) parse to JsonObject
             val root = Json.parseToJsonElement(jsonText).jsonObject
 
-            // 3) extrai e desserializa "fields"
+            // 3) extracts and deserializes "fields"
             val fieldsJson = root["fields"]!!.jsonArray
             val fields: List<Field> = Json.decodeFromString(fieldsJson.toString())
 
-            // 4) extrai e desserializa "sections"
+            // 4) extracts and deserializes "sections"
             val sectionsJson = root["sections"]!!.jsonArray
             val sections: List<Section> = Json.decodeFromString(sectionsJson.toString())
 
-            // 5) grava no banco
+            // 5)record in the bank
             fieldDao.insertAll(fields)
             sectionDao.insertAll(sections)
         }
